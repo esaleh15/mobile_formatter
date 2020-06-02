207a1f5fe0c97d75dd528dc904b4ecd35df9d8fb
Elliot Saleh <elisaleh1@hotmail.co.uk>
2020-06-02 10:14:11 +0100

    add remove spacing checks for number

--- lib/formatter/phone_number/uk.rb
+++ lib/formatter/phone_number/uk.rb
@@ -1,9 +1,12 @@
 module Formatter
 	module PhoneNumber
 		module UK
-			def self.format
-				# binding.pry
-				'hello'
+			def self.format(number)
+				removed_space_no = remove_space(number)
+			end
+
+			def self.remove_space(number) # check for spaces
+				number.match(/\s+/) ? number.gsub(/\s+/, "") : number
 			end
 		end
 	end
--- spec/formatter/phone_number/uk_spec.rb
+++ spec/formatter/phone_number/uk_spec.rb
@@ -1,6 +1,50 @@
 require 'spec_helper'
+require 'pry'
 require 'formatter/phone_number/uk'
 
 describe Formatter::PhoneNumber::UK do
 
+	let(:formatter) { Formatter::PhoneNumber::UK }
+
+	describe 'Verifing Spaces' do
+
+		context 'when number has one space' do
+
+			let (:number) {'+447321 890230'}
+
+			it 'should remove and join together' do
+
+				f = formatter.remove_space(number)
+				expect(f).to eq('+447321890230')
+			end
+		end
+
+		context 'when number has multiple spaces' do
+
+			let (:number) {'+4473 21 8902 30'}
+
+			it 'should remove and join together' do
+
+				f = formatter.remove_space(number)
+				expect(f).to eq('+447321890230')
+			end
+		end
+
+		context 'when number has no spaces' do
+
+			let (:number) {'+447321890230'}
+
+			it 'should return the original number' do
+
+				f = formatter.remove_space(number)
+				expect(f).to eq('+447321890230')
+			end
+		end
+
+		describe 'Validity' do
+			context 'Output' do
+
+			end
+		end
+	end
 end
