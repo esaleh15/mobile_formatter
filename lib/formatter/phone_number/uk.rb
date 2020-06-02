e0f738876b5042e8ef3f5141d95fee4c10221668
Elliot Saleh <elisaleh1@hotmail.co.uk>
2020-06-02 08:45:24 +0100

    inital setup

---
+++ lib/formatter/phone_number/uk.rb
@@ -0,0 +1,10 @@
+module Formatter
+	module PhoneNumber
+		module UK
+			def self.format
+				# binding.pry
+				'hello'
+			end
+		end
+	end
+end
---
+++ spec/formatter/phone_number/uk_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'formatter/phone_number/uk'
+
+describe Formatter::PhoneNumber::UK do
+
+end
